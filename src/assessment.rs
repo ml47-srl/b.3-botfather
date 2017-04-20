11f3cceea5e1638ee6cc3a8aea3e9a8c8044a384
memoryleak47 <memoryleak47@protonmail.ch>
2017-04-20 13:39:26 +0200

    removed assessment; added StopReason

--- src/assessment.rs
+++
@@ -1,6 +0,0 @@
-pub enum Stop { Timeout, Win, Lose }
-
-pub struct Assessment {
-	pub stop : Stop,
-	pub time : i64
-}
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,9 @@ pub extern crate libsrl;
 use libsrl::db::Database;
 use libsrl::cell::Cell;
 
-mod assessment;
-use assessment::Assessment;
+pub enum StopReason { Win, Fail, Timeout }
 
 pub trait Botfather {
 	fn call(db : &mut Database, target : &Cell);
-	fn assess(assessment : &Assessment);
+	fn assess(stop_reason : StopReason, milliseconds : u32);
 }
